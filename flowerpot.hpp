7d0f5ce898397dfbccf0d201b7dcdf7b2e56c676
oasjow <sliu3@zagmail.gonzaga.edu>
2024-03-19 14:02:22 -0700

    -

---
+++ flowerpot.hpp
@@ -0,0 +1,14 @@
+#ifndef FLOWERPOT_HPP
+#define FLOWERPOT_HPP
+#include "trapozoid.hpp"
+#include<string>
+
+class FlowerPot {
+    private:
+    Trapezoid* base
+    string flower_type
+    float price;
+
+    public:
+
+}
