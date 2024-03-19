90274248656e48aa1b1edc38263f030cea58caa3
oasjow <149857156+oasjow@users.noreply.github.com>
2024-03-19 21:36:24 +0000

    -

--- flowerpot.cpp
+++ flowerpot.cpp
@@ -6,19 +6,35 @@ void FlowerPot::set_base(Trapezoid* input) {
     }
     base = input;
 }
+
 void FlowerPot::set_flower_type(std::string input) {
     flower_type = input;
 }
+
 void FlowerPot::set_price(float input) {
     price = input;
 }
+
 Trapezoid* FlowerPot::get_base() const {
     return base;
 }
+
 std::string FlowerPot::get_flower_type() const {
     return flower_type;
 }
+
 float FlowerPot::get_price() const {
     return price;
 }
 
+FlowerPot::FlowerPot() {
+    base = new Trapezoid;
+    flower_type = "None";
+    price = 0;
+}
+
+FlowerPot::~FlowerPot() {
+    if (base != nullptr) {
+        delete base;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include"rectangle.hpp"
 #include"trapezoid.hpp"
+#include"flowerpot.hpp"
 
 int main() {
     Rectangle* rect = new Rectangle;
@@ -18,6 +19,17 @@ int main() {
     std::cout << "Height: " << trap->get_height() << std::endl;
     std::cout << "Area: " << trap->find_area() << std::endl;
     delete trap;
+
+    FlowerPot* pot = new FlowerPot;
+    pot->set_base(new Trapezoid(5, 10, 5));
+    pot->set_flower_type("Rose");
+    pot->set_price(10);
+    std::cout << "Base 1: " << pot->get_base()->get_base1() << std::endl;
+    std::cout << "Base 2: " << pot->get_base()->get_base2() << std::endl;
+    std::cout << "Height: " << pot->get_base()->get_height() << std::endl;
+    std::cout << "Flower Type: " << pot->get_flower_type() << std::endl;
+    std::cout << "Price: " << pot->get_price() << std::endl;
+    delete pot;
 
     return 0;
 }
